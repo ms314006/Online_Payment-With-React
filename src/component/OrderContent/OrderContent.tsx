4891bd80c5314e6dcfbecfb01a36aafe1546c86b
GQSM <ms314006@hotmail.com>
2019-08-02 23:15:10 +0800

    [ADD] 完成 OrderContent 的內容

--- src/component/OrderContent/OrderContent.tsx
+++ src/component/OrderContent/OrderContent.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
   orderPaper: {
+    boxSizing: 'border-box',
     backgroundColor: '#F3F3F3',
     height: '281px',
+    padding: '16px',
+  },
+  item: {
+    margin: '16px 0px',
+  },
+  title: {
+    color: '#5E5E5E',
+  },
+  content: {
+    color: '#000000',
   }
 };
 
@@ -14,7 +26,21 @@ const OrderContent = (props: any) => {
   const { classes, className } = props;
   return (
     <Paper className={clsx(classes.orderPaper, className)}>
-      12313
+      <Typography variant="h6" component="h5" >
+        訂單資訊
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom className={clsx(classes.item, className)}>
+        <div className={clsx(classes.title, className)}>商品名稱：</div>
+        <div className={clsx(classes.content, className)}>Iphone XR手機殼 x1</div>
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom className={clsx(classes.item, className)}>
+        <div className={clsx(classes.title, className)}>訂單編號：</div>
+        <div className={clsx(classes.content, className)}>17485739</div>
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom className={clsx(classes.item, className)}>
+        <div className={clsx(classes.title, className)}>訂單金額：</div>
+        <div className={clsx(classes.content, className)}>NT$ 600</div>
+      </Typography>
     </Paper>
   )
 }
