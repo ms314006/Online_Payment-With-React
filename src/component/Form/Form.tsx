f7e8197a7bd75132e59bf0f2df216c30ff93a452
GQSM <ms314006@hotmail.com>
2019-08-02 23:13:26 +0800

    [ADD] 將選擇付款類別加入 Form 頁面中

--- src/component/Form/Form.tsx
+++ src/component/Form/Form.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import PayTypeItems from './PayTypeItems';
 
 const styles = {
   formBlock: {
+    boxSizing: 'border-box',
+    padding: '36px',
     height: '100%',
     boxShadow: '0px 2px 13px 0px rgba(0,0,0,0.08)',
+  },
+  formTitleBlock: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  formTitle: {
+    background: 'linear-gradient(0deg, #B5FFE4 45%, #FFFFFF 55%)',
+    width: '236px',
+    textAlign: 'center',
+  },
+  formContent: {
+    marginTop: '80px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   }
 };
 
@@ -14,7 +33,14 @@ const OrderContent = (props: any) => {
   const { classes, className } = props;
   return (
     <Paper className={clsx(classes.formBlock, className)}>
-      12313
+      <div className={clsx(classes.formTitleBlock, className)}>
+        <Typography variant="h6" component="h5" className={clsx(classes.formTitle, className)}>
+          STEP1. 選擇付款方式
+        </Typography>
+      </div>
+      <div className={clsx(classes.formContent, className)}>
+        <PayTypeItems />
+      </div>
     </Paper>
   )
 }
